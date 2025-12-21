0e679ba135a0f867e861c2cf708dc94dea4cdefa
kusht <107963246+KushTurner@users.noreply.github.com>
2025-12-21 23:29:40 +0000

    Added POST statement

--- internal/http/handlers/transactions.go
+++ internal/http/handlers/transactions.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"encoding/base64"
+	"io"
+	"log/slog"
 	"net/http"
 	"time"
 
 	"github.com/kushturner/finances/internal/json"
+	"github.com/kushturner/finances/internal/statements"
 	"github.com/kushturner/finances/internal/transactions"
 )
 
@@ -17,12 +21,38 @@ type transactionResponse struct {
 
 type transactionHandler struct {
 	service transactions.Service
+	llm     statements.LLMClient
 }
 
-func NewTransactionHandler(service transactions.Service) *transactionHandler {
+func NewTransactionHandler(service transactions.Service, llm statements.LLMClient) *transactionHandler {
 	return &transactionHandler{
 		service: service,
+		llm:     llm,
 	}
+}
+
+func (h *transactionHandler) AddTransactions(w http.ResponseWriter, r *http.Request) {
+
+	fileBytes, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		slog.Error("Failed to read statement file", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	transactions, err := h.llm.ReadStatement(r.Context(), base64.StdEncoding.EncodeToString(fileBytes))
+
+	if err != nil {
+		slog.Error("LLM Failed to read statement", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	h.service.AddTransactions(r.Context(), transactions)
+
+	json.Encode(w, http.StatusAccepted, "Added transactions successfully")
 }
 
 func (h *transactionHandler) ListTransactions(w http.ResponseWriter, r *http.Request) {
