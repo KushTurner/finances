56c10d37c490a58d933314e21de3e139c8cb8334
KushTurner <107963246+KushTurner@users.noreply.github.com>
2026-01-21 02:23:07 +0000

    Refactored so router takes interfaces

--- internal/server/router.go
+++ internal/server/router.go
@@ -2,17 +2,16 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kushturner/finances/internal/csvparser"
-	"github.com/kushturner/finances/internal/db"
 	"github.com/kushturner/finances/internal/handlers"
 	"github.com/kushturner/finances/internal/transaction"
 )
 
-func NewRouter(querier db.Querier) *chi.Mux {
+func NewRouter(transactionService transaction.Service, parserService csvparser.Service) *chi.Mux {
 	r := chi.NewRouter()
 
-	parserService := csvparser.NewService()
-	transactionService := transaction.NewService(querier)
+	r.Use(middleware.Logger)
 
 	r.Get("/transactions", handlers.NewListTransactionsHandler(transactionService))
 	r.Post("/transactions/upload", handlers.NewUploadTransactionsHandler(transactionService, parserService))
