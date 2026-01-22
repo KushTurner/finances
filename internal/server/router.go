1b466fd658daa6781aa54a914300c1bded06c831
KushTurner <107963246+KushTurner@users.noreply.github.com>
2026-01-22 21:57:56 +0000

    Added wildcard CORS config

--- internal/server/router.go
+++ internal/server/router.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/kushturner/finances/internal/csvparser"
 	"github.com/kushturner/finances/internal/handlers"
 	"github.com/kushturner/finances/internal/transaction"
@@ -10,6 +11,13 @@ import (
 
 func NewRouter(transactionService transaction.Service, parserService csvparser.Service) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Content-Type"},
+		AllowCredentials: false,
+	}))
 
 	r.Use(middleware.Logger)
 
